ba8bcf454eea07d0a57692f9564e5e690a251762
ramadeviganireddy@gmail.com <ramadeviganireddy@gmail.com>
2023-03-02 05:47:28 +0000

    commit-1

--- rama-demo.py
+++ rama-demo.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-result = dbutils.notebook.run("/Repos/ganireddy.ramadevi+uc@databricks.com/databricks-test-repo/rama-db-test-00286889_1", 7200)
+result = dbutils.notebook.run("rama-db-test-00286889_1", 7200)
 print(result)
 
 # COMMAND ----------
