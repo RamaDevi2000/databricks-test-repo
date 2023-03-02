10ab9abdcf3734ab3c67a3ccf16abb23105fac3f
ramadeviganireddy@gmail.com <ramadeviganireddy@gmail.com>
2023-03-02 05:08:35 +0000

    demo-test

---
+++ rama-db-test-00286889_1.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+!pwd
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC pip install paramiko
+
+# COMMAND ----------
+
+pip install --upgrade pip
+
+# COMMAND ----------
+
+
